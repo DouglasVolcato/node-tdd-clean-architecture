c53a147628c3786bdf5e70ed47c82a252934f6e9
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-23 23:15:00 -0300

    feat: create CreateUserController

---
+++ src/presentation/controllers/create-user-controller.ts
@@ -0,0 +1,36 @@
+import {
+  ControllerInputType,
+  ControllerInterface,
+  ControllerOutputType,
+  ValidatorInterface,
+} from "../abstract";
+import { CreateUserServiceInterface, UserDtoType, UserEntityType } from "@/domain/abstract";
+import { badRequest, ok, serverError } from "../helpers";
+
+export class CreateUserController implements ControllerInterface {
+  private readonly validator: ValidatorInterface;
+  private readonly createUserService: CreateUserServiceInterface;
+
+  public constructor(
+    validator: ValidatorInterface,
+    createUserService: CreateUserServiceInterface
+  ) {
+    this.validator = validator;
+    this.createUserService = createUserService;
+  }
+
+  public async execute(
+    request: ControllerInputType<UserDtoType>
+  ): Promise<ControllerOutputType<UserEntityType | Error>> {
+    try {
+      const error = this.validator.validate(request);
+      if (error) {
+        return badRequest(error);
+      }
+      const user = await this.createUserService.execute(request);
+      return ok(user);
+    } catch (error) {
+      return serverError();
+    }
+  }
+}
