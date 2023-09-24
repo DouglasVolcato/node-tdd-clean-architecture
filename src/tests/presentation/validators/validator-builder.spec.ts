8eeda6f321b34a4f2b65669f4dd43e0e19ea6772
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-24 11:10:52 -0300

    test: ensure isRequired and isEmail add field name inside object

--- src/tests/presentation/validators/validator-builder.spec.ts
+++ src/tests/presentation/validators/validator-builder.spec.ts
@@ -2,6 +2,7 @@ import { ValidatorInterface } from "@/presentation/abstract";
 
 class ValidatorBuilder implements ValidatorInterface {
   private requiredFields: string[] = [];
+  private emailFields: string[] = [];
 
   public validate(request: any): Error | undefined {
     throw new Error("Method not implemented.");
@@ -11,26 +12,31 @@ class ValidatorBuilder implements ValidatorInterface {
     this.requiredFields.push(fieldName);
   }
 
+  public isEmail(fieldName: string): void {
+    this.emailFields.push(fieldName);
+  }
 }
 
 describe("ValidatorBuilder", () => {
-  it("Should add new required field names by calling isRequired", () => {
-    const sut = new ValidatorBuilder();
-    const addRequiredFieldSpy = jest.spyOn(sut, "isRequired");
-    sut.isRequired("required_field");
-
-    expect((sut as any).requiredFields).toContain("required_field");
-    expect(addRequiredFieldSpy).toBeCalledTimes(1);
-  });
-
   it("Should add multiple field names by calling isRequired multiple times", () => {
     const sut = new ValidatorBuilder();
-    const addRequiredFieldSpy = jest.spyOn(sut, "isRequired");
+    const isRequiredSpy = jest.spyOn(sut, "isRequired");
     sut.isRequired("required_field_1");
     sut.isRequired("required_field_2");
 
     expect((sut as any).requiredFields).toContain("required_field_1");
     expect((sut as any).requiredFields).toContain("required_field_2");
-    expect(addRequiredFieldSpy).toBeCalledTimes(2);
+    expect(isRequiredSpy).toBeCalledTimes(2);
+  });
+
+  it("Should add multiple email field names by calling isEmail multiple times", () => {
+    const sut = new ValidatorBuilder();
+    const isEmailSpy = jest.spyOn(sut, "isEmail");
+    sut.isEmail("email_field_1");
+    sut.isEmail("email_field_2");
+
+    expect((sut as any).emailFields).toContain("email_field_1");
+    expect((sut as any).emailFields).toContain("email_field_2");
+    expect(isEmailSpy).toBeCalledTimes(2);
   });
 });
