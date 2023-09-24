cd9489786cee7953d012e7446773c04537a382fa
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-23 23:14:45 -0300

    test: ensure createUserController is working

--- src/tests/presentation/controllers/create-user-controller.spec.ts
+++ src/tests/presentation/controllers/create-user-controller.spec.ts
@@ -1,43 +1,12 @@
-class RequiredFieldError extends Error {
-  public constructor(fieldName: string) {
-    super(`${fieldName} is missing`);
-    this.name = "RequiredFieldError";
-  }
-}
-
-interface ValidatorInterface {
-  validate(): Error | undefined;
-}
-
-const badRequest = (error: Error): ControllerOutput<Error> => ({
-  statusCode: 400,
-  data: error,
-});
-
-class CreateUserController {
-  private readonly validator: ValidatorInterface;
-
-  public constructor(validator: ValidatorInterface) {
-    this.validator = validator;
-  }
-
-  public async execute(
-    request: ControllerInput<any>
-  ): Promise<ControllerOutput<any> | any> {
-    const error = this.validator.validate();
-
-    if (error) {
-      return badRequest(error);
-    }
-  }
-}
-
-type ControllerInput<T> = T;
-
-type ControllerOutput<T> = {
-  statusCode: number;
-  data: T;
-};
+import {
+  CreateUserServiceInterface,
+  UserDtoType,
+  UserEntityType,
+} from "@/domain/abstract";
+import { ValidatorInterface } from "@/presentation/abstract";
+import { CreateUserController } from "@/presentation/controllers";
+import { RequiredFieldError, ServerError } from "@/presentation/errors";
+import { makeUserDto, makeUserEntity, throwError } from "@/tests/test-helpers";
 
 class ValidatorStub implements ValidatorInterface {
   validate(): Error | undefined {
@@ -45,27 +14,88 @@ class ValidatorStub implements ValidatorInterface {
   }
 }
 
+class CreateUserServiceStub implements CreateUserServiceInterface {
+  public async execute(userDto: UserDtoType): Promise<UserEntityType> {
+    return makeUserEntity();
+  }
+}
+
+type SutTypes = {
+  sut: CreateUserController;
+  validatorStub: ValidatorInterface;
+  createUserServiceStub: CreateUserServiceInterface;
+};
+
+const makeSut = (): SutTypes => {
+  const validatorStub = new ValidatorStub();
+  const createUserServiceStub = new CreateUserServiceStub();
+  const sut = new CreateUserController(validatorStub, createUserServiceStub);
+
+  return { validatorStub, createUserServiceStub, sut };
+};
+
 describe("CreateUserController", () => {
   it("Should call validator once", async () => {
-    const validatorStub = new ValidatorStub();
-    const sut = new CreateUserController(validatorStub);
+    const { sut, validatorStub } = makeSut();
     const validatorSpy = jest.spyOn(validatorStub, "validate");
-    await sut.execute({});
+    await sut.execute(makeUserDto());
 
     expect(validatorSpy).toBeCalledTimes(1);
   });
 
   it("Should return a bad request if validator returns an error", async () => {
-    const validatorStub = new ValidatorStub();
-    const sut = new CreateUserController(validatorStub);
-
+    const { sut, validatorStub } = makeSut();
     jest
       .spyOn(validatorStub, "validate")
       .mockReturnValueOnce(new RequiredFieldError("any_field"));
-
-    const response = await sut.execute({});
+    const response = await sut.execute(makeUserDto());
 
     expect(response.statusCode).toBe(400);
     expect(response.data).toEqual(new RequiredFieldError("any_field"));
+  });
+
+  it("Should return a server error if validator throws", async () => {
+    const { sut, validatorStub } = makeSut();
+    jest
+      .spyOn(validatorStub, "validate")
+      .mockImplementationOnce(() => throwError());
+
+    const response = await sut.execute(makeUserDto());
+
+    expect(response.statusCode).toBe(500);
+    expect(response.data).toBeInstanceOf(ServerError);
+  });
+
+  it("Should call create user service once with correct values", async () => {
+    const { sut, createUserServiceStub } = makeSut();
+    const serviceSpy = jest.spyOn(createUserServiceStub, "execute");
+    await sut.execute(makeUserDto());
+
+    expect(serviceSpy).toHaveBeenCalledWith(makeUserDto());
+    expect(serviceSpy).toBeCalledTimes(1);
+  });
+
+  it("Should return ok with the new user created", async () => {
+    const { sut, createUserServiceStub } = makeSut();
+    jest
+      .spyOn(createUserServiceStub, "execute")
+      .mockReturnValueOnce(Promise.resolve(makeUserEntity()));
+
+    const response = await sut.execute(makeUserDto());
+
+    expect(response.statusCode).toBe(200);
+    expect(response.data).toEqual(makeUserEntity());
+  });
+
+  it("Should return a server error if create user service throws", async () => {
+    const { sut, createUserServiceStub } = makeSut();
+    jest
+      .spyOn(createUserServiceStub, "execute")
+      .mockImplementationOnce(() => throwError());
+
+    const response = await sut.execute(makeUserDto());
+
+    expect(response.statusCode).toBe(500);
+    expect(response.data).toBeInstanceOf(ServerError);
   });
 });
