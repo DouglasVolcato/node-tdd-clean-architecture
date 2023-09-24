959c9bdbd3048e2ceebe5700f704e282295c5854
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-23 21:39:01 -0300

    test: ensure CreateUserController  returns a bad request if validator returns an error

--- src/tests/presentation/controllers/create-user-controller.spec.ts
+++ src/tests/presentation/controllers/create-user-controller.spec.ts
@@ -1,32 +1,71 @@
-interface ValidatorInterface{
-    validate(): boolean
+class RequiredFieldError extends Error {
+  public constructor(fieldName: string) {
+    super(`${fieldName} is missing`);
+    this.name = "RequiredFieldError";
+  }
 }
 
-class CreateUserController{
-    private readonly validator: ValidatorInterface
-
-    public constructor(validator:ValidatorInterface){
-        this.validator = validator
-    }
-
-    public async execute(){
-        this.validator.validate()
-    }
+interface ValidatorInterface {
+  validate(): Error | undefined;
 }
 
-class ValidatorStub implements ValidatorInterface{
-    validate(): boolean {
-        return true
+const badRequest = (error: Error): ControllerOutput<Error> => ({
+  statusCode: 400,
+  data: error,
+});
+
+class CreateUserController {
+  private readonly validator: ValidatorInterface;
+
+  public constructor(validator: ValidatorInterface) {
+    this.validator = validator;
+  }
+
+  public async execute(
+    request: ControllerInput<any>
+  ): Promise<ControllerOutput<any> | any> {
+    const error = this.validator.validate();
+
+    if (error) {
+      return badRequest(error);
     }
+  }
 }
 
-describe('CreateUserController', () => {
-    it('Should call validator once',async () => {
-        const validatorStub = new ValidatorStub()
-        const sut = new CreateUserController(validatorStub)
-        const validatorSpy = jest.spyOn(validatorStub, 'validate')
-        await sut.execute()
+type ControllerInput<T> = T;
 
-        expect(validatorSpy).toBeCalledTimes(1)
-    })
-})
+type ControllerOutput<T> = {
+  statusCode: number;
+  data: T;
+};
+
+class ValidatorStub implements ValidatorInterface {
+  validate(): Error | undefined {
+    return;
+  }
+}
+
+describe("CreateUserController", () => {
+  it("Should call validator once", async () => {
+    const validatorStub = new ValidatorStub();
+    const sut = new CreateUserController(validatorStub);
+    const validatorSpy = jest.spyOn(validatorStub, "validate");
+    await sut.execute({});
+
+    expect(validatorSpy).toBeCalledTimes(1);
+  });
+
+  it("Should return a bad request if validator returns an error", async () => {
+    const validatorStub = new ValidatorStub();
+    const sut = new CreateUserController(validatorStub);
+
+    jest
+      .spyOn(validatorStub, "validate")
+      .mockReturnValueOnce(new RequiredFieldError("any_field"));
+
+    const response = await sut.execute({});
+
+    expect(response.statusCode).toBe(400);
+    expect(response.data).toEqual(new RequiredFieldError("any_field"));
+  });
+});
