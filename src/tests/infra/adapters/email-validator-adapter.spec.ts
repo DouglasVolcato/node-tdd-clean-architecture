c43ac2f0ddfed2a1bb7a6f88a6c2125b8e35adcf
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-24 22:20:26 -0300

    test: ensure EmailValidator is working

---
+++ src/tests/infra/adapters/email-validator-adapter.spec.ts
@@ -0,0 +1,49 @@
+import { EmailValidatorAdapter } from "@/infra/adapters";
+import { throwError } from "@/tests/test-helpers";
+import { validate } from "email-validator";
+
+jest.mock("email-validator", () => ({
+  validate: jest.fn(),
+}));
+
+type SutTypes = {
+  sut: EmailValidatorAdapter;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new EmailValidatorAdapter();
+  return { sut };
+};
+
+describe("EmailValidatorAdapter", () => {
+  it("Should call validate with correct value", () => {
+    const { sut } = makeSut();
+    sut.isEmail("any_email");
+
+    expect(validate).toHaveBeenCalledTimes(1);
+    expect(validate).toHaveBeenCalledWith("any_email");
+  });
+
+  it("Should return true if validate returns true", () => {
+    const { sut } = makeSut();
+    (validate as jest.Mock).mockReturnValueOnce(true);
+    const output = sut.isEmail("any_email");
+
+    expect(output).toBeTruthy();
+  });
+
+  it("Should return false if validate returns false", () => {
+    const { sut } = makeSut();
+    (validate as jest.Mock).mockReturnValueOnce(false);
+    const output = sut.isEmail("any_email");
+
+    expect(output).toBeFalsy();
+  });
+
+  it("Should throw if validate throws", () => {
+    const { sut } = makeSut();
+    (validate as jest.Mock).mockImplementationOnce(() => throwError());
+
+    expect(() => sut.isEmail("any_email")).toThrow();
+  });
+});
