5c0638b816e76917d95fb510bd07858fa1658f6e
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-24 21:29:31 -0300

    test: ensure IdGeneratorAdapter is working

---
+++ src/tests/infra/adapters/id-generator-adapter.spec.ts
@@ -0,0 +1,33 @@
+import { IdGeneratorAdapter } from "@/infra/adapters";
+import { v4 } from "uuid";
+
+jest.mock("uuid", () => ({
+    v4: jest.fn()
+}));
+
+type SutTypes = {
+  sut: IdGeneratorAdapter;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new IdGeneratorAdapter();
+  return { sut };
+};
+
+describe("IdGeneratorAdapter", () => {
+  it("Should call uuid library", () => {
+    const { sut } = makeSut();
+    sut.generateId();
+
+    expect(v4).toHaveBeenCalledTimes(1);
+  });
+
+  it("Should return generated uuid", () => {
+    const { sut } = makeSut();
+    (v4 as jest.Mock).mockReturnValueOnce("generated_id");
+    const generatedUuid = sut.generateId();
+
+    expect(generatedUuid).toBe("generated_id");
+  });
+});
+
