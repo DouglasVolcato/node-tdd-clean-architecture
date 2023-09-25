2abe1a9ab4a5bd4c30c1ede665db2057aaf22c7f
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-24 21:29:11 -0300

    feat: add id generator adapter

---
+++ src/infra/adapters/id-generator-adapter.ts
@@ -0,0 +1,8 @@
+import { IdGeneratorInterface } from "@/domain/abstract";
+import { v4 } from "uuid";
+
+export class IdGeneratorAdapter implements IdGeneratorInterface {
+  public generateId(): string {
+    return v4();
+  }
+}
