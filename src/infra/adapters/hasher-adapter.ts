65f890fc2932eba9cabe3b900d2c3fbf23467cbd
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-24 21:49:42 -0300

    feat: add hasher adapter

---
+++ src/infra/adapters/hasher-adapter.ts
@@ -0,0 +1,14 @@
+import { HasherInterface } from "@/domain/abstract";
+import { hashSync } from "bcrypt";
+
+export class HasherAdapter implements HasherInterface {
+  private readonly hashSalt: number;
+
+  public constructor(hashSalt: number) {
+    this.hashSalt = hashSalt;
+  }
+
+  public hash(value: string): string {
+    return hashSync(value, this.hashSalt);
+  }
+}
--- src/infra/adapters/index.ts
+++ src/infra/adapters/index.ts
@@ -1,1 +1,2 @@
-export * from './id-generator-adapter'
+export * from "./id-generator-adapter";
+export * from "./hasher-adapter";
