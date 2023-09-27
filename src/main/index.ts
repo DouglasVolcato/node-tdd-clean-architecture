1608f25eb3be2e4080c2bff95aced79160a61399
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-27 18:57:01 -0300

    feat: add database connection on app start

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,10 @@
+import { DatabaseConnector } from "infra/database";
 import { FrameWorkAdapter } from "./adapters";
 import { Env, routes } from "./config";
 
 const framework = new FrameWorkAdapter(routes, Env.PORT);
-framework.start();
+const databaseConnector = DatabaseConnector;
+
+databaseConnector.connect(Env.MONGO_URL).then(() => {
+  framework.start();
+});
