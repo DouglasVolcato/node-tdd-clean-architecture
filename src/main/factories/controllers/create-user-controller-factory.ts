3d2fc05c34643e51f01d88eacdd0ce0abe93dbc6
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-26 21:42:37 -0300

    feat: add CreateUserControllerFactory

---
+++ src/main/factories/controllers/create-user-controller-factory.ts
@@ -0,0 +1,18 @@
+import { CreateUserService } from "@/domain/services";
+import { HasherAdapter, IdGeneratorAdapter } from "@/infra/adapters";
+import { UserRepository } from "@/infra/database";
+import { CreateUserController } from "@/presentation/controllers";
+import { ValidatorComposite } from "@/presentation/validators";
+
+export function makeCreateUserControllerFactory(): CreateUserController {
+  const createUserRepository = new UserRepository();
+  const idGenerator = new IdGeneratorAdapter();
+  const hasher = new HasherAdapter(10);
+  const createUserService = new CreateUserService(
+    createUserRepository,
+    idGenerator,
+    hasher
+  );
+  const validatorComposite = new ValidatorComposite();
+  return new CreateUserController(createUserService, validatorComposite);
+}
--- src/presentation/controllers/index.ts
+++ src/presentation/controllers/index.ts
@@ -1,1 +1,2 @@
-export * from './create-user-controller'
+export * from "./create-user-controller";
+export * from "./controller";
