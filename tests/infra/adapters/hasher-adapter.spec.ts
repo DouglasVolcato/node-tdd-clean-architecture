9a35f7e9d85dbc2a32084f342c5fc5da5a985ac4
Douglas Volcato <douglasvolcato@gmail.com>
2023-09-30 23:02:57 -0300

    test: ensure hash validation is working

--- tests/infra/adapters/hasher-adapter.spec.ts
+++ tests/infra/adapters/hasher-adapter.spec.ts
@@ -1,9 +1,10 @@
 import { HasherAdapter } from "../../../src/infra/adapters";
 import { throwError } from "../../../tests/test-helpers";
-import { hashSync } from "bcrypt";
+import { hashSync, compareSync } from "bcrypt";
 
 jest.mock("bcrypt", () => ({
   hashSync: jest.fn(),
+  compareSync: jest.fn(),
 }));
 
 type SutTypes = {
@@ -18,26 +19,61 @@ const makeSut = (): SutTypes => {
 };
 
 describe("HasherAdapter", () => {
-  it("Should call hash method with correct value", () => {
-    const { sut, hashSalt } = makeSut();
-    sut.hash("any_value");
+  describe("hash", () => {
+    it("Should call hash method with correct value", () => {
+      const { sut, hashSalt } = makeSut();
+      sut.hash("any_value");
 
-    expect(hashSync).toHaveBeenCalledWith("any_value", hashSalt);
-    expect(hashSync).toHaveBeenCalledTimes(1);
+      expect(hashSync).toHaveBeenCalledWith("any_value", hashSalt);
+      expect(hashSync).toHaveBeenCalledTimes(1);
+    });
+
+    it("Should return the generated hash", () => {
+      const { sut } = makeSut();
+      (hashSync as jest.Mock).mockReturnValueOnce("generated_hash");
+      const generatedHash = sut.hash("any_value");
+
+      expect(generatedHash).toBe("generated_hash");
+    });
+
+    it("Should throw if bcrypt throws", () => {
+      const { sut } = makeSut();
+      (hashSync as jest.Mock).mockImplementationOnce(() => throwError());
+
+      expect(() => sut.hash("any_value")).toThrow();
+    });
   });
 
-  it("Should return the generated hash", () => {
-    const { sut } = makeSut();
-    (hashSync as jest.Mock).mockReturnValueOnce("generated_hash");
-    const generatedHash = sut.hash("any_value");
+  describe("validate", () => {
+    it("Should call validate method with correct value", () => {
+      const { sut } = makeSut();
+      sut.validate("any_value", "hashed_value");
 
-    expect(generatedHash).toBe("generated_hash");
-  });
+      expect(compareSync).toHaveBeenCalledWith("any_value", "hashed_value");
+      expect(compareSync).toHaveBeenCalledTimes(1);
+    });
 
-  it("Should throw if bscrypt throws", () => {
-    const { sut } = makeSut();
-    (hashSync as jest.Mock).mockImplementationOnce(() => throwError());
+    it("Should return true if bcrypt returns true", () => {
+      const { sut } = makeSut();
+      (compareSync as jest.Mock).mockReturnValueOnce(true);
+      const generatedHash = sut.validate("any_value", "hashed_value");
 
-    expect(() => sut.hash("any_value")).toThrow();
+      expect(generatedHash).toBeTruthy();
+    });
+
+    it("Should return true if bcrypt returns true", () => {
+      const { sut } = makeSut();
+      (compareSync as jest.Mock).mockReturnValueOnce(false);
+      const generatedHash = sut.validate("any_value", "hashed_value");
+
+      expect(generatedHash).toBeFalsy();
+    });
+
+    it("Should throw if bcrypt throws", () => {
+      const { sut } = makeSut();
+      (compareSync as jest.Mock).mockImplementationOnce(() => throwError());
+
+      expect(() => sut.validate("any_value", "hashed_value")).toThrow();
+    });
   });
 });
