72da72ea89b52b0fd9670f0abd42f9e114fb268f
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-01 12:00:22 -0300

    test: ensure login route is woring

--- tests/main/routes/login-routes.test.ts
+++ tests/main/routes/login-routes.test.ts
@@ -1,10 +1,11 @@
 import { FrameWorkAdapter } from "../../../src/main/adapters";
 import { Env } from "../../../src/main/config";
-import { LoginDtoType } from "../../../src/domain/abstract";
-import { DatabaseConnector } from "../../../src/infra/database";
+import { LoginDtoType, UserEntityType } from "../../../src/domain/abstract";
+import { DatabaseConnector, UserModel } from "../../../src/infra/database";
 import { Express } from "express";
 import request from "supertest";
 import { loginRoutes } from "../../../src/main/routes";
+import { HasherAdapter } from "../../../src/infra/adapters";
 
 const route = "/login";
 const databaseConnector = new DatabaseConnector();
@@ -13,6 +14,13 @@ let app: Express;
 
 const makeValidLoginDto = (): LoginDtoType => ({
   password: "Test123",
+  email: "douglasvolcato@gmail.com",
+});
+
+const makeValidUserDto = (): UserEntityType => ({
+  id: "23h9f82hf892h8",
+  name: "Douglas",
+  password: new HasherAdapter(10).hash("Test123"),
   email: "douglasvolcato@gmail.com",
 });
 
@@ -28,16 +36,37 @@ describe("Login routes", () => {
   afterAll(async () => {
     await frameworkAdapter.stop();
     await databaseConnector.disconnect();
-    jest.setTimeout(30000)
+    jest.setTimeout(30000);
+  });
+
+  beforeEach(async () => {
+    await UserModel.deleteMany({});
   });
 
   describe(`POST ${route}`, () => {
-    test("Should return 200 with the generated token", async () => {
-      const requestBody = makeValidLoginDto();
+    test("Should return 400 if user does not exist", async () => {
+      const response = await request(app).post(route).send(makeValidLoginDto());
 
-      const response = await request(app).post(route).send(requestBody);
+      expect(response.statusCode).toBe(400);
+    });
+
+    test("Should return 200 with the generated token", async () => {
+      const newUser = new UserModel(makeValidUserDto());
+      await newUser.save();
+      const response = await request(app).post(route).send(makeValidLoginDto());
 
       expect(response.statusCode).toBe(200);
+      expect(response.body.token).toBeDefined();
+    });
+
+    test("Should return 400 if password is invalid", async () => {
+      const newUser = new UserModel(makeValidUserDto());
+      await newUser.save();
+      const response = await request(app)
+        .post(route)
+        .send({ ...makeValidLoginDto(), password: "invalid_password" });
+
+      expect(response.statusCode).toBe(400);
     });
 
     test("Should return 400 if does not receive password", async () => {
