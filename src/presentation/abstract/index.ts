825206c707d4a995d3f3a384642192e984f604e5
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-02 19:35:53 -0300

    refactor: move controllers to their own directory

--- src/presentation/abstract/index.ts
+++ src/presentation/abstract/index.ts
@@ -1,6 +1,7 @@
 export * from "./validator/validator-interface";
-export * from "./controller/input-type";
-export * from "./controller/output-type";
-export * from "./controller/controller-interface";
+export * from "./controllers/input-type";
+export * from "./controllers/output-type";
+export * from "./controllers/controller-interface";
+export * from "./middlewares/middleware-interface";
 export * from "./validator/validator-builder-interface";
 export * from "./validator/email-validator-interface ";
