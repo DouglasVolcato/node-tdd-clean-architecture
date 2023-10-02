7565215b0d9446e344974e35406182eb650a1727
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-01 21:13:48 -0300

    test: ensure DeleteUserRepository is working

--- tests/infra/repositories/user-repository.spec.ts
+++ tests/infra/repositories/user-repository.spec.ts
@@ -77,10 +77,44 @@ describe("UserRepository", () => {
     it("Should throw if mongoose throws", async () => {
       const { sut } = makeSut();
       jest
-        .spyOn(UserModel.prototype, "save")
+        .spyOn(UserModel, "findOne")
         .mockImplementationOnce(() => throwError());
 
-      await expect(() => sut.create(makeUserEntity())).rejects.toThrow();
+      await expect(
+        async () => await sut.getByEmail(makeUserEntity().email)
+      ).rejects.toThrow();
+    });
+  });
+
+  describe("GetById", () => {
+    it("Should return a user", async () => {
+      const { sut } = makeSut();
+      const userEntity = makeUserEntity();
+      await sut.create(userEntity);
+      const foundUser = await sut.getById(userEntity.id);
+
+      expect(foundUser.id).toBeDefined();
+      expect(foundUser.name).toBe(userEntity.name);
+      expect(foundUser.email).toBe(userEntity.email);
+      expect(foundUser.password).toBe(userEntity.password);
+    });
+
+    it("Should return undefined if user was not found", async () => {
+      const { sut } = makeSut();
+      const foundUser = await sut.getById(makeUserEntity().id);
+
+      expect(foundUser).toBeUndefined();
+    });
+
+    it("Should throw if mongoose throws", async () => {
+      const { sut } = makeSut();
+      jest
+        .spyOn(UserModel, "findOne")
+        .mockImplementationOnce(() => throwError());
+
+      await expect(
+        async () => await sut.getById(makeUserEntity().id)
+      ).rejects.toThrow();
     });
   });
 });
