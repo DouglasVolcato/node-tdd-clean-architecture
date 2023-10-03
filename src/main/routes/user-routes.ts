c16391a657f8d5c0773b443a6b762e2082990ecf
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-02 22:40:30 -0300

    chore: add delete user route

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,10 +1,18 @@
+import { makeDeleteUserControllerFactory } from "main/factories/controllers/delete-user-controller-factory";
 import { RouteDtoType, RouteEnumType } from "../abstract";
 import { makeCreateUserControllerFactory } from "../factories";
+import { makeUserAuthMiddlewareFactory } from "main/factories/middlewares/user-auth-middleware-factory";
 
 export const userRoutes: RouteDtoType[] = [
   {
     type: RouteEnumType.POST,
     url: "/user/create",
     controller: makeCreateUserControllerFactory(),
+  },
+  {
+    type: RouteEnumType.DELETE,
+    url: "/user/delete/:id",
+    controller: makeDeleteUserControllerFactory(),
+    middleware: makeUserAuthMiddlewareFactory(),
   },
 ];
