27a48c9873984b760bb739b4baca28c03b6cfd07
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-04 22:02:25 -0300

    test: ensure ErrorLogControllerDecorator is working

---
+++ tests/presentation/decorators/error-log-controller-decorator.spec.ts
@@ -0,0 +1,123 @@
+import { ErrorLogControllerDecorator } from "../../../src/presentation/decorators";
+import { ErrorLogServiceInterface } from "../../../src/domain/abstract";
+import {
+  ControllerInterface,
+  ControllerOutputType,
+} from "../../../src/presentation/abstract";
+
+class ControllerStub implements ControllerInterface {
+  public async execute(request: any): Promise<ControllerOutputType<any>> {
+    return Promise.resolve({
+      statusCode: 200,
+      data: {},
+    });
+  }
+}
+
+class ErrorLogServiceStub implements ErrorLogServiceInterface {
+  public async execute(error: Error): Promise<void> {
+    return;
+  }
+}
+
+const makeRequest = (): any => ({
+  property1: "any_value",
+  property2: "any_value",
+});
+
+type SutTypes = {
+  sut: ErrorLogControllerDecorator;
+  controllerStub: ControllerInterface;
+  errorLogServiceStub: ErrorLogServiceInterface;
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = new ControllerStub();
+  const errorLogServiceStub = new ErrorLogServiceStub();
+  const sut = new ErrorLogControllerDecorator(
+    controllerStub,
+    errorLogServiceStub
+  );
+  return { sut, controllerStub, errorLogServiceStub };
+};
+
+describe("ErrorLogControllerDecorator", () => {
+  it("Should call controller with correct values", async () => {
+    const { sut, controllerStub } = makeSut();
+    const controllerSpy = jest.spyOn(controllerStub, "execute");
+    await sut.execute(makeRequest());
+
+    expect(controllerSpy).toHaveBeenCalledTimes(1);
+    expect(controllerSpy).toHaveBeenCalledWith(makeRequest());
+  });
+
+  it("Should return what the controller returns", async () => {
+    const { sut, controllerStub } = makeSut();
+    const controllerResponse = {
+      statusCode: 200,
+      data: {
+        value: "any_value",
+      },
+    };
+    jest
+      .spyOn(controllerStub, "execute")
+      .mockReturnValueOnce(Promise.resolve(controllerResponse));
+    const response = await sut.execute(makeRequest());
+
+    expect(response).toEqual(controllerResponse);
+  });
+
+  it("Should call ErrorLogService with correct values if status code is 500 and data is an error", async () => {
+    const { sut, controllerStub, errorLogServiceStub } = makeSut();
+    const controllerResponse = {
+      statusCode: 500,
+      data: new Error("any_error"),
+    };
+    jest
+      .spyOn(controllerStub, "execute")
+      .mockReturnValueOnce(Promise.resolve(controllerResponse));
+    const errorLogServiceSpy = jest.spyOn(errorLogServiceStub, "execute");
+    await sut.execute(makeRequest());
+
+    expect(errorLogServiceSpy).toHaveBeenCalledTimes(1);
+    expect(errorLogServiceSpy).toHaveBeenCalledWith(controllerResponse.data);
+  });
+
+  it("Should not call ErrorLogService if status code is not 500 and data is an error", async () => {
+    const { sut, controllerStub, errorLogServiceStub } = makeSut();
+    const controllerResponse = {
+      statusCode: 200,
+      data: new Error("any_error"),
+    };
+    jest
+      .spyOn(controllerStub, "execute")
+      .mockReturnValueOnce(Promise.resolve(controllerResponse));
+    const errorLogServiceSpy = jest.spyOn(errorLogServiceStub, "execute");
+    await sut.execute(makeRequest());
+
+    expect(errorLogServiceSpy).not.toHaveBeenCalledTimes(1);
+    expect(errorLogServiceSpy).not.toHaveBeenCalledWith(
+      controllerResponse.data
+    );
+  });
+
+  it("Should not call ErrorLogService if status code is 500 but data is not an error", async () => {
+    const { sut, controllerStub, errorLogServiceStub } = makeSut();
+    const controllerResponse = {
+      statusCode: 200,
+      data: {
+        value: "any_value",
+      },
+    };
+    jest
+      .spyOn(controllerStub, "execute")
+      .mockReturnValueOnce(Promise.resolve(controllerResponse));
+    const errorLogServiceSpy = jest.spyOn(errorLogServiceStub, "execute");
+    await sut.execute(makeRequest());
+
+    expect(errorLogServiceSpy).not.toHaveBeenCalledTimes(1);
+    expect(errorLogServiceSpy).not.toHaveBeenCalledWith(
+      controllerResponse.data
+    );
+  });
+});
