711a2254b0d7ecf9cc57d791d956551a277c557f
Douglas Volcato <douglasvolcato@gmail.com>
2023-10-04 22:11:04 -0300

    fix: delete unused code

--- src/infra/adapters/file-handler-adapter.ts
+++ src/infra/adapters/file-handler-adapter.ts
@@ -1,10 +1,6 @@
 import { FileWritterInterface } from "../../../src/domain/abstract";
 import { writeFile } from "fs/promises";
 
-jest.mock("fs/promises", () => ({
-  writeFile: jest.fn(),
-}));
-
 export class FileHandlerAdapter implements FileWritterInterface {
   public async writeInFile(filePath: string, content: string): Promise<void> {
     return await writeFile(filePath, content);
